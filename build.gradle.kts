bdb7e8f5809473f5b28abf54b744ce96bf9fe23b
jinnyjinnyjinjin <kkam2ya@gmail.com>
2021-06-28 15:25:24 +0900

    Add dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.kafka:spring-kafka")
